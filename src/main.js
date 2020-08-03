1d94d85b123ac717015d13e8b895aec825524ded
adam <adam.brown@digital.justice.gov.uk>
2020-08-03 22:44:39 +0100

    Init commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// https://getbootstrap.com/docs/4.0/getting-started/download/#npm
+require('bootstrap')
+
 Vue.config.productionTip = false
 
 new Vue({
