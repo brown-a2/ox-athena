3df31bc57d0b13d1d816243452c1d2fe9bffaa46
adam <adam.brown@digital.justice.gov.uk>
2020-08-03 22:52:30 +0100

    Add correct github address

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:brown-a2/ox-athena.git master:gh-pages
 
 cd -
